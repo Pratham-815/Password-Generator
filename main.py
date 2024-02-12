9861aba463cabc1718ff7e0a7cf4b4ea329bc641
Pratham Agarwal <prathamagarwal815@gmail.com>
2024-02-12 20:23:40 +0530

    taking user input for the password setup

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+print("Welcome to the Password Generator")
+
+nr_letters = int(input("How many letters would you like in your password ? : "))
+nr_numbers = int(input("How many numbers would you like in your password ? : "))
+nr_symbols = int(input("How many symbols would you like in your password ? : "))
+
+
